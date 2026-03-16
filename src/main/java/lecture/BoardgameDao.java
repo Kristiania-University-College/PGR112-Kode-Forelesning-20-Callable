7bc8ef22a9d9c40e2f4f299044d74376b59303e9
algorithminator <frederic.dorn@kristiania.no>
2026-03-16 11:00:15 +0100

    demo callable

--- src/main/java/lecture/BoardgameDao.java
+++ src/main/java/lecture/BoardgameDao.java
@@ -14,7 +14,8 @@ public class BoardgameDao {
      */
     private final Connection conn;
     private static final String GET_BOARDGAME_SQL = "SELECT brettspill_id, navn, type, antall_spillere, spilletid, aldersgrense, bilde FROM Brettspill WHERE brettspill_id=?";
-    private static final String ADD_BOARDGAME_NO_ID_SQL = "INSERT INTO Brettspill (navn, type, antall_spillere, spilletid, aldersgrense, bilde) VALUES(?,?,?,?,?,?)";
+    private static final String CALL_ADD_BOARD_GAME = "{call addBoardGame(?,?,?,?,?,?,?)}";
+    //private static final String ADD_BOARDGAME_NO_ID_SQL = "INSERT INTO Brettspill (navn, type, antall_spillere, spilletid, aldersgrense, bilde) VALUES(?,?,?,?,?,?)";
 
     public BoardgameDao(Connection conn) {
         this.conn = conn;
@@ -43,11 +44,10 @@ public class BoardgameDao {
         }
     }
 
-
-    // auto-inkrement
-    public int addBoardGame(String name, String type, int nrOfPlayers, int minutes, int ageLimit, String imageUrl) throws SQLException {
+    //callable
+    public int addBoardGameUsingStoredProcedure(String name, String type, int nrOfPlayers, int minutes, int ageLimit, String imageUrl) throws SQLException {
         try (
-             PreparedStatement statement = conn.prepareStatement(ADD_BOARDGAME_NO_ID_SQL, Statement.RETURN_GENERATED_KEYS);
+             CallableStatement statement = conn.prepareCall(CALL_ADD_BOARD_GAME);
         ) {
             statement.setString(1, name);
             statement.setString(2, type);
@@ -55,48 +55,13 @@ public class BoardgameDao {
             statement.setInt(4, minutes);
             statement.setInt(5, ageLimit);
             statement.setString(6, imageUrl);
-            int rowsUpdated = statement.executeUpdate();
-            if(rowsUpdated==1){
-                try(ResultSet keys = statement.getGeneratedKeys()){
-                    if(keys.next()){
-                        return keys.getInt(1);
-                    }
-                }
-            }
-        }
-        return 0;
-    }
-
-    // transaction
-    public void addBoardGames(List<BoardGame> newBoardGames) throws SQLException {
-        try (
-             PreparedStatement statement = conn.prepareStatement(ADD_BOARDGAME_NO_ID_SQL, Statement.RETURN_GENERATED_KEYS);
-        ) {
-            boolean autoCommit = conn.getAutoCommit();
-            conn.setAutoCommit(false);
-            try{
-                for (BoardGame newBoardGame : newBoardGames) {
-                    //statement.setInt(1, newBoardGame.id());
-                    statement.setString(1, newBoardGame.name());
-                    statement.setString(2, newBoardGame.type());
-                    statement.setInt(3, newBoardGame.nrOfPlayers());
-                    statement.setInt(4, newBoardGame.minutes());
-                    statement.setInt(5, newBoardGame.ageLimit());
-                    statement.setString(6, newBoardGame.imageUrl()); //demonstrate wrong index
-                    statement.executeUpdate();
-                }
-                conn.commit();
-            } catch (SQLException sqle){
-                System.out.println("Exception caught. Rolling back transaction.");
-                conn.rollback();
-                throw sqle;
-            } finally {
-                // Setting the commit mode to what it was originally.
-                conn.setAutoCommit(autoCommit);
-            }
-
+            statement.registerOutParameter(7, Types.INTEGER);
+            statement.executeUpdate();
+            return statement.getInt(7);
         }
     }
+
+
 
 
 
---
+++ src/main/java/lecture/Program.java
@@ -0,0 +1,45 @@
+package lecture;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Program {
+
+
+    /*
+     * DAO - business logic
+     */
+    private final BoardgameDao bgDao;
+
+    public Program(BoardgameDao bgDao) {
+        this.bgDao = bgDao;
+    }
+
+
+    void useOptional(BoardgameDao bgDao, int i) throws SQLException {
+        Optional<BoardGame> bg = bgDao.getBoardGame(5);
+        bg.ifPresent(boardGame -> IO.println(boardGame.name()));
+    }
+
+    void useStoredProcedure(BoardgameDao bgDao) throws SQLException {
+        IO.println("Trying to add new board game using callable statement");
+        int generatedId = bgDao.addBoardGameUsingStoredProcedure("Citadels", "Strategi", 7, 120, 10, "citadels.jpg");
+        IO.println("Result:"+generatedId);
+        useOptional(bgDao, 5);
+    }
+
+
+
+
+    public void run() throws SQLException {
+        // Optional
+        useOptional(bgDao, 5);
+
+        // CallAble
+        useStoredProcedure(bgDao);
+
+    }
+
+}
